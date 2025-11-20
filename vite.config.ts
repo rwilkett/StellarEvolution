bf22485c798b7026baa72dd62f45860a316e8337
Ray Wilkett <ray.wilkett@intimetec.com>
2025-11-19 17:50:45 -0700

    Add base with repository name to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/StellarEvolution/',
   plugins: [react()],
   test: {
     globals: true,
